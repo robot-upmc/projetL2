f495c2c515448b5136bc983a69973c817a6ef6d5
ASSIREM <assirem200@live.fr>
2020-02-02 17:47:29 +0100

    test

---
+++ robot.py
@@ -0,0 +1,12 @@
+class Robot:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        
+    def deplacer(self,x,y):
+        self.x+=x
+        self.y+=y
+        
+    def changdirec(self,direction):
+        if(direction=="droite"){}
+        
